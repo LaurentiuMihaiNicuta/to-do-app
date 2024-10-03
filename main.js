0a84fde106f13d26af2fdde04b090a0235f529a3
LaurentiuMihaiNicuta <laurentiumihai.nicuta07@gmail.com>
2024-10-03 12:09:14 +0300

    added the code

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+//main.js
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+import './src/css/style.css'
+import { createSidebar } from '/src/js/side-bar';
+import { createMainContent } from '/src/js/content';
+
+
+
+function createAppUI() {
+    const appContainer = document.getElementById('app');
+    appContainer.className = 'app-container';
+
+    
+    const {mainContent, mainContentBottom, mainContentTop} = createMainContent();
+    const sideBar = createSidebar(mainContentBottom,mainContentTop);
+
+    appContainer.appendChild(sideBar);
+    appContainer.appendChild(mainContent);
+
+    console.log('Ui created')
+}
+
+createAppUI();
